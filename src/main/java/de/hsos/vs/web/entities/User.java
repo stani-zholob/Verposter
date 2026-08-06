c4b00622cb674f5f57431e56b35c1b4cf32a2620
lukkas1 <ooochs.lukas.privat@gmail.com>
2026-08-06 17:18:33 +0200

    Datenbank angefangen

--- src/main/java/de/hsos/vs/web/entities/User.java
+++ src/main/java/de/hsos/vs/web/entities/User.java
@@ -2,6 +2,40 @@ package de.hsos.vs.web.entities;
 
 public class User {
     private int id;
-    private String username;
+    private String name;
+    /** Wird in der Spalte "password" abgelegt - dort gehoert ein Hash hinein, kein Klartext. */
     private String passwordHash;
+
+    public User() {
+    }
+
+    public User(int id, String name, String passwordHash) {
+        this.id = id;
+        this.name = name;
+        this.passwordHash = passwordHash;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
 }
