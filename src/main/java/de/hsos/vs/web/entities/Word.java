d10aae9906083afc90eced81adf7d08da813f07b
lukkas1 <ooochs.lukas.privat@gmail.com>
2026-08-10 10:49:31 +0200

    Entities kontrolliert

--- src/main/java/de/hsos/vs/web/entities/Word.java
+++ src/main/java/de/hsos/vs/web/entities/Word.java
@@ -1,39 +1,50 @@
 package de.hsos.vs.web.entities;
 
 public class Word {
-    int id;
-    int  topicId;
-    String name;
-    String tip;
-    public Word(int id,int topicId, String name, String tip) {
+    private int id;
+    private int topicId;
+    private String word;
+    private String tip;
+
+    public Word() {
+    }
+
+    public Word(int id, int topicId, String word, String tip) {
         this.id = id;
         this.topicId = topicId;
-        this.name = name;
-        this.tip = tip;
-    }
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public int getTopicId() {
-        return topicId;
-    }
-    public void setTopicId(int topicId) {
-        this.topicId = topicId;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getTip() {
-        return tip;
-    }
-    public void setTip(String tip) {
+        this.word = word;
         this.tip = tip;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(int topicId) {
+        this.topicId = topicId;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getTip() {
+        return tip;
+    }
+
+    public void setTip(String tip) {
+        this.tip = tip;
+    }
 }
